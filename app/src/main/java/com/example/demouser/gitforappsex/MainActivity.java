271cdb00dcc5803363e20a275b37a4f13fc6bd02
nault22e <emnault15@gmail.com>
2019-01-15 14:00:39 -0500

    change 2

--- app/src/main/java/com/example/demouser/gitforappsex/MainActivity.java
+++ app/src/main/java/com/example/demouser/gitforappsex/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Change 1
+        //Change 2
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
